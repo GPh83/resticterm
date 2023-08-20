5919aeae3ceee6889cc86c428b54aa91a239f22a
Philippe GRAILLE <philippe.graille@micro-be.com>
2023-08-20 17:27:03 +0200

    Add help option on command line #17

--- resticterm/Program.cs
+++ resticterm/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Terminal.Gui;
 
 
@@ -20,22 +22,53 @@ namespace resticterm
         // Entry point
         static void Main(string[] args)
         {
-            Application.Init();
+            if (HasHelp(args))
+            {
+                DisplayHelp();
+            }
+            else
+            {
+                Application.Init();
 
-            // Retrieve necessary data
-            dataManager = new Models.DataManager();
-            dataManager.Start();
+                // Retrieve necessary data
+                dataManager = new Models.DataManager();
+                dataManager.Start();
 
-            // Initialize repo manager
-            restic = new Restic.Restic(dataManager.config.RepoPath, dataManager.config.EncryptedRepoPassword);
+                // Initialize repo manager
+                restic = new Restic.Restic(dataManager.config.RepoPath, dataManager.config.EncryptedRepoPassword);
 
-            // Default Master password 
-            dataManager.config.MasterPassword = "8YSZm5bIaWdN6Itd";
+                // Default Master password 
+                dataManager.config.MasterPassword = "8YSZm5bIaWdN6Itd";
 
-            // Start UI
-            var main = new Views.UI_Main();
-            main.Create();
-            Application.Run();
+                // Start UI
+                var main = new Views.UI_Main();
+                main.Create();
+                Application.Run();
+            }
+        }
+
+        // Help 
+        static bool HasHelp(string[] args)
+        {
+            bool ret = false;
+
+            ret |= Array.IndexOf(args, "-h") >= 0;
+            ret |= Array.IndexOf(args, "--help") >= 0;
+            ret |= Array.IndexOf(args, "-v") >= 0;
+            ret |= Array.IndexOf(args, "--version") >= 0;
+            return ret;
+        }
+
+        static void DisplayHelp()
+        {
+            Console.WriteLine("resticterm version : " + Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            Console.WriteLine("");
+            Console.WriteLine("resticterm is a multi-platform console UI for restic backup software. (https://restic.net/).");
+            Console.WriteLine("It can be used alone as backup tools or with restic command line for manage existing repository.");
+            Console.WriteLine("");
+            //Console.WriteLine("config file can be in current directory or in home directory");
+            Console.WriteLine("config file location : " + Path.Combine(Directory.GetCurrentDirectory(), "recticterm.config.json"));
+            Console.WriteLine("");
         }
     }
 }
