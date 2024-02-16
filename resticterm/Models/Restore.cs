dce8180b3e81ddfdd578fe86fc6bf3f26adc0c6b
Philippe GRAILLE <philippe.graille@micro-be.com>
2024-02-16 21:08:48 +0100

    Restore Model

---
+++ resticterm/Models/Restore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resticterm.Models
+{
+    class Restore
+    {
+        // Common 
+        public string message_type { get; set; }
+        public int total_files { get; set; }
+        public int total_bytes { get; set; }
+        
+        // Only "message_type":"status"
+        public int percent_done { get; set; }
+
+        // Only "message_type":"summary"
+        public int files_restored { get; set; }
+        public int bytes_restored { get; set; }
+    }
+
+}
